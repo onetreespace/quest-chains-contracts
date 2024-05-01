277cd6d672eec23f865884dfe94067ab216f8d66
dan13ram <dan13ram@gmail.com>
2024-05-01 21:45:37 +0530

    deployed to sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,12 +19,14 @@ task('accounts', 'Prints the list of accounts', async (_args, hre) => {
   }
 });
 
-if (!process.env.PRIVATE_KEY) {
-  console.error('invalid env variable: PRIVATE_KEY');
+if (!process.env.PRIVATE_KEY && !process.env.MNEMONIC) {
+  console.error('invalid env variable: PRIVATE_KEY or MNEMONIC');
   process.exit(1);
 }
 
-const accounts = [process.env.PRIVATE_KEY!];
+const accounts = process.env.MNEMONIC
+  ? { mnemonic: process.env.MNEMONIC }
+  : [process.env.PRIVATE_KEY!];
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -44,10 +46,6 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
-      accounts,
-    },
     optimism: {
       url: 'https://mainnet.optimism.io',
       accounts,
@@ -60,16 +58,12 @@ const config: HardhatUserConfig = {
       url: 'https://rpc-mainnet.maticvigil.com',
       accounts,
     },
-    polygonMumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
-      accounts,
-    },
     arbitrumOne: {
       url: 'https://arb1.arbitrum.io/rpc',
       accounts,
     },
-    arbitrumGoerli: {
-      url: 'https://goerli-rollup.arbitrum.io/rpc',
+    sepolia: {
+      url: 'https://1rpc.io/sepolia',
       accounts,
     },
   },
@@ -79,13 +73,11 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.ETHERSCAN_API_KEY!,
       optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY!,
       polygon: process.env.POLYGONSCAN_API_KEY!,
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY!,
       gnosis: process.env.GNOSISSCAN_API_KEY!,
       arbitrumOne: process.env.ARBISCAN_API_KEY!,
-      arbitrumGoerli: process.env.ARBISCAN_API_KEY!,
+      sepolia: process.env.ETHERSCAN_API_KEY!,
     },
   },
   typechain: {
--- scripts/utils/index.ts
+++ scripts/utils/index.ts
@@ -3,24 +3,22 @@ import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 
 export const NETWORK_NAME: Record<number, string> = {
-  5: 'Goerli Testnet',
   10: 'Optimism',
   100: 'Gnosis Chain',
   137: 'Polygon Mainnet',
   42161: 'Arbitrum One',
-  421613: 'Arbitrum Goerli Testnet',
-  80001: 'Polygon Mumbai Testnet',
+  11155111: 'Sepolia Testnet',
+  17000: 'Holesky Testnet',
   31337: 'Hardhat Chain',
 };
 
 export const NETWORK_CURRENCY: Record<number, string> = {
-  5: 'GoerliETH',
   10: 'ETH',
   100: 'xDAI',
   137: 'MATIC',
   42161: 'ETH',
-  421613: 'GoerliETH',
-  80001: 'MATIC',
+  11155111: 'SEPETH',
+  17000: 'HOLETH',
   31337: 'HardhatETH',
 };
 
@@ -33,25 +31,23 @@ export type DeploymentInfo = {
 };
 
 export const TREASURY_ADDRESS: Record<string, string> = {
-  5: '0xC9F2D9adfa6C24ce0D5a999F2BA3c6b06E36F75E',
-  10: '0xC9F2D9adfa6C24ce0D5a999F2BA3c6b06E36F75E', // todo: replace with gnosis safe when introducing fees
+  10: '0x19a8eb80c1483CEAA1278B16C5D5eF0104F85905', // dan13.eth
   100: '0xcDba6263aC0a162848380A1eD117B48D973EABFC', // gnosis safe
   137: '0xcDba6263aC0a162848380A1eD117B48D973EABFC', // gnosis safe
-  42161: '0xC9F2D9adfa6C24ce0D5a999F2BA3c6b06E36F75E', // todo: replace with gnosis safe when introducing fees
-  421613: '0xC9F2D9adfa6C24ce0D5a999F2BA3c6b06E36F75E',
-  80001: '0xC9F2D9adfa6C24ce0D5a999F2BA3c6b06E36F75E',
+  42161: '0x19a8eb80c1483CEAA1278B16C5D5eF0104F85905', // dan13.eth
+  17000: '0x19a8eb80c1483CEAA1278B16C5D5eF0104F85905', // dan13.eth
+  11155111: '0x19a8eb80c1483CEAA1278B16C5D5eF0104F85905', // dan13.eth
   31337: '0xffffffffffffffffffffffffffffffffffffffff',
 };
 
 export const PAYMENT_TOKEN: Record<string, string> = {
-  5: '0x7f8F6E42C169B294A384F5667c303fd8Eedb3CF3', // custom USDC
   10: '0x7f5c764cbc14f9669b88837ca1490cca17c31607', // USDC
   100: '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83', // USDC
   137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
   42161: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8', // USDC
-  421613: '0x67F4C72a50f8Df6487720261E188F2abE83F57D7', // custom USDC
-  80001: '0xD60e36945160281eB456A962408295B1FC257796', // custom USDC
   31337: '0xffffffffffffffffffffffffffffffffffffffff',
+  11155111: '0x57B9f2c192BBfa5CAbc79A683435990FEa665861', // TOKEN
+  17000: '0x59730da9b5f93fe1e1fd7d62f94b787ecc7feef1', // TOKEN
 };
 
 // export const DEFAULT_UPGRADE_FEE = 10000000; // 10 USDC with 6 decimals
